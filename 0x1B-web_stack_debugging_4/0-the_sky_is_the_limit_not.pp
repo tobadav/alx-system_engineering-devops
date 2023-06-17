295e7b84a4b2121ec8b89f72ea021cc7aaa14075
tobadav <tobadav@gmail.com>
2023-06-17 13:54:24 +0100

    debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,9 @@
-# Increases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# fix the limit nginx.
+exec { 'myfix':
+command => 'sed -i "/ULIMIT=/c\ULIMIT=\"-n 2000\"" /etc/default/nginx',
+path    => '/bin',
+}
+service { 'nginx':
+ensure    => running,
+subscribe => Exec['myfix'],
 }
